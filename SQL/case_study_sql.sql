75ce1d7d192f72bc84e2a2a017bb780492f6c4fa
TrongNhan7 <batrongnhandng@gmail.com>
2022-07-17 17:55:47 +0700

    up

--- SQL/case_study_sql.sql
+++ SQL/case_study_sql.sql
@@ -476,8 +476,34 @@ FROM
 GROUP BY ten_dich_vu_di_kem
 HAVING COUNT(dvdk.ma_dich_vu_di_kem) = 1;
 
--- 
+-- 15.	Hiển thi thông tin của tất cả nhân viên bao gồm ma_nhan_vien,
+-- ho_ten, ten_trinh_do, ten_bo_phan, so_dien_thoai, dia_chi mới chỉ
+-- lập được tối đa 3 hợp đồng từ năm 2020 đến 2021 --
 
+SELECT 
+    nhan_vien.ma_nhan_vien AS ma_nhan_vien,
+    hop_dong.ma_hop_dong,
+    nhan_vien.ho_ten AS ho_ten,
+    ten_trinh_do,
+    ten_bo_phan,
+    nhan_vien.so_dien_thoai AS so_dien_thoai,
+    nhan_vien.dia_chi AS dia_chi
+FROM
+    hop_dong
+        JOIN
+    nhan_vien ON hop_dong.ma_nhan_vien = nhan_vien.ma_nhan_vien
+        JOIN
+    trinh_do ON nhan_vien.ma_trinh_do = trinh_do.ma_trinh_do
+        JOIN
+    bo_phan ON nhan_vien.ma_bo_phan = bo_phan.ma_bo_phan
+        AND hop_dong.ma_nhan_vien IN (SELECT 
+            ma_nhan_vien
+        FROM
+            hop_dong
+        WHERE
+            YEAR(ngay_lam_hop_dong) BETWEEN 2020 AND 2021
+        GROUP BY ma_nhan_vien
+        HAVING COUNT(ma_hop_dong) <= 3)
 
 
 
