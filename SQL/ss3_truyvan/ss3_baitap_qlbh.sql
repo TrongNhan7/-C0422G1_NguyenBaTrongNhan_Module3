c85d037971afd46919805a7265675c556f28ba8f
TrongNhan7 <batrongnhandng@gmail.com>
2022-07-19 09:25:18 +0700

    up

--- SQL/ss3_truyvan/ss3_baitap_qlbh.sql
+++ SQL/ss3_truyvan/ss3_baitap_qlbh.sql
@@ -1,34 +1,34 @@
 drop database if exists ss3_baitap_qlbh;
 
 create database ss3_baitap_qlbh;
-use ss3_baitap_qlbh;
+use quan_ly_ban_hang;
 
 -- Hiển thị các thông tin  gồm oID, oDate, oPrice của tất cả các hóa đơn trong bảng `Order`--
 SELECT 
     oID,oDate,oTotalPrice
 FROM
-    quan_ly_ban_hang.`order`;
+    `order`;
 
 -- Hiển thị danh sách các khách hàng đã mua hàng, và danh sách sản phẩm được mua bởi các khách --
 SELECT 
     C.cName, P.pName
 FROM
-    quan_ly_ban_hang.customer C
+   customer C
         INNER JOIN
-    quan_ly_ban_hang.`order` O ON C.cID = O.cID
+    `order` O ON C.cID = O.cID
         INNER JOIN
-    quan_ly_ban_hang.orderDetail OD ON O.oID = OD.oID_detail
+    orderDetail OD ON O.oID = OD.oID_detail
         INNER JOIN
-    quan_ly_ban_hang.Product P ON OD.pID_detail = P.pID;
+    Product P ON OD.pID_detail = P.pID;
     
     
 -- Hiển thị tên những khách hàng không mua bất kỳ một sản phẩm nào --   
 SELECT 
     C.cName
 FROM
-    quan_ly_ban_hang.customer C
+    customer C
         LEFT JOIN
-    quan_ly_ban_hang.`order` O ON C.cID = O.cID
+   `order` O ON C.cID = O.cID
 WHERE
     O.oDate IS NULL;
     
@@ -37,11 +37,11 @@ WHERE
 SELECT 
     O.oID, O.oDate, SUM(odQTY * pPrice)
 FROM
-    quan_ly_ban_hang.`order` O
+    `order` O
         JOIN
-    quan_ly_ban_hang.orderDetail OD ON O.oID = OD.oID_detail
+   orderDetail OD ON O.oID = OD.oID_detail
         INNER JOIN
-    quan_ly_ban_hang.Product P ON OD.pID_detail = P.pID
+    Product P ON OD.pID_detail = P.pID
 GROUP BY oID;
 
 
--- SQL/ss4/baitap.sql
+++ SQL/ss4/baitap.sql
@@ -1,19 +1,32 @@
 use quanlysinhvien;
 
-SELECT 
-    `subject`.SubId,
-    `subject`.SubName,
-    MAX(Credit) AS max_credit
-FROM
-    `subject`;
-  
-SELECT 
-    `subject`.SubId, `subject`.SubName, MAX(Mark) AS max_mark
-FROM
-    mark
-        JOIN
-    `subject` ON `subject`.SubId = mark.SubId; 
+--  Hiển thị tất cả các thông tin môn học (bảng subject) có credit lớn nhất.--
 
+SELECT 
+    *
+FROM
+    `subject` s
+WHERE
+    credit = (SELECT 
+            MAX(credit)
+        FROM
+            `subject`);
+
+-- Hiển thị các thông tin môn học có điểm thi lớn nhất. --
+
+SELECT 
+    sub.*, mark AS 'điểm cao nhất'
+FROM
+    mark m
+        RIGHT JOIN
+    subject sub ON m.subid = sub.subid
+WHERE
+    m.mark = (SELECT 
+            MAX(mark.mark)
+        FROM
+            mark);
+            
+--  Hiển thị các thông tin sinh viên và điểm trung bình của mỗi sinh viên, xếp hạng theo thứ tự điểm giảm dần--
 
 SELECT 
     student.StudentId, student.StudentName, AVG(Mark)
