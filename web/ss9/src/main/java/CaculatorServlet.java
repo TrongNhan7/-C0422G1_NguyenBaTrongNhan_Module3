97dbcf7d13d4e52cf7fb2014320e0bb0ca8de29c
TrongNhan7 <batrongnhandng@gmail.com>
2022-07-28 09:05:32 +0700

    up

--- web/ss9/src/main/java/CaculatorServlet.java
+++ web/ss9/src/main/java/CaculatorServlet.java
@@ -9,13 +9,13 @@ import java.io.IOException;
 @WebServlet(name = "CaculatorServlet",urlPatterns = {"/discount-percent"})
 public class CaculatorServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        double number1 = Double.parseDouble(request.getParameter("list_price"));
-        double number2 = Double.parseDouble(request.getParameter("discount_percent"));
-        double discount_amount = number1*number2*0.01;
-        double discount_price = number1-discount_amount;
+        double listPrice = Double.parseDouble(request.getParameter("list_price"));
+        double discountPercent = Double.parseDouble(request.getParameter("discount_percent"));
+        double discountAmount = listPrice*discountPercent*0.01;
+        double discountPrice = listPrice-discountAmount;
         RequestDispatcher requestDispatcher = request.getRequestDispatcher("result.jsp");
-        request.setAttribute("discount_amount",discount_amount);
-        request.setAttribute("discount_price",discount_price);
+        request.setAttribute("discount_amount",discountAmount);
+        request.setAttribute("discount_price",discountPrice);
         requestDispatcher.forward(request,response);
     }
 
